83cfc7f58c886a86638bbc62f55f9f9a38623c8a
brianramaswami <brianramaswami@yahoo.com>
2017-06-15 20:37:23 -0700

    save me

--- app/data/reservations.js
+++ app/data/reservations.js
@@ -1,4 +1,14 @@
 module.exports = {
-    tables:[],
-    waitlist:[]
+    tables:[{
+        "customerName": "bob",
+        "phoneNumber": "1",
+        "customerEmail": "LDkfj@yahoo.com",
+        "customerID" : "655544"
+    }],
+    waitlist:[{
+        "customerName": "Sam",
+        "phoneNumber": "15",
+        "customerEmail": "Sam@yahoo.com",
+        "customerID" : "365309"
+    }]
 };
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,4 +1,20 @@
 var data = require('./../data/reservations.js');
 
 module.exports = (app,path)=>{
+    app.get("/api/tables", function(req, res){
+        res.json(data.tables);
+    });
+
+    app.get("/api/waitlist", function(req, res){
+        res.json(data.waitlist);
+    });
+
+    app.post("/api/tables", function(req, res){
+        var newReservation = req.params.body;
+        // if(data.tables.length > 5){
+            
+        // }
+    });
+        
+    
 };
